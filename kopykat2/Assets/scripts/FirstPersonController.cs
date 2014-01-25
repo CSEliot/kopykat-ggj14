9ce44b522beb8ed32c7f6dfedc8ed5ef6fa94a40
unknown <eliot2@ECS005APC09.ad.umbc.edu>
2014-01-25 10:14:50 -0500

    fixed dawg

--- kopykat2/Assets/scripts/FirstPersonController.cs
+++ kopykat2/Assets/scripts/FirstPersonController.cs
@@ -17,11 +17,15 @@ public class FirstPersonController : MonoBehaviour {
 	CharacterController characterController;
 	public bool newState;
 	public string oldState;
-	public List<string> states = new List<string>();
-	states.Add
+	private bool testy = true;
+    private List<string> states = new List<string>();
+
+
+
 			
 	// Use this for initialization
 	void Start () {
+        states.Add("standing");states.Add("walking");states.Add("jumping");states.Add("hands up");
 		Screen.lockCursor = true;
 		characterController = GetComponent<CharacterController>();
 		if (!characterController) {
@@ -33,50 +37,57 @@ public class FirstPersonController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
-		//test for change in state
-		if (oldState != state) 
-		{
-			newState = true;
-		}
 		//player rotation
 		//left and right
 		float rotLeftRight = Input.GetAxis("Mouse X")*mouseSensetivity;
+        Debug.Log("rotLeftRight = " + rotLeftRight);
 		transform.Rotate(0, rotLeftRight, 0);
 		//record old state and clear state for change
 		oldState = state;
-		state.Clear ();
 
 		//Movement
 		float forwardSpeed = Input.GetAxis("Vertical");
 		float sideSpeed = Input.GetAxis("Horizontal");
 		//Debug.Log ("FORWARD SPEED: "+forwardSpeed ); Debug.Log ("SIDE SPEED"+sideSpeed);
 		//add the new states
-		state.Add (forwardSpeed);
-		state.Add (sideSpeed);
 
 
 		verticalVelocity += Physics.gravity.y * Time.deltaTime;
 
 		if (characterController.isGrounded && Input.GetButtonDown("Jump")){
-			verticalVelocity = jumpSpeed;		
+			verticalVelocity = jumpSpeed;
+            state = states[2];
+            newState = true;
 		}
 
 		Vector3 speed = new Vector3( sideSpeed*movementSpeed, verticalVelocity*.50f, forwardSpeed*movementSpeed);
 		
 		speed = transform.rotation * speed;
-
+        if (speed != Vector3.zero || rotLeftRight != 0)
+        {
+            state = states[1];
+            newState = true;
+        }
+        else if (!newState && speed == Vector3.zero && rotLeftRight == 0)
+        {
+            state = states[0];
+            newState = true;
+        }
 		characterController.Move( speed * Time.deltaTime);
+        if (newState)
+        {
+            Debug.Log(state);
+        }
 	}
 
-	public ArrayList getState(){
+	public string getState(){
 		//Debug.Log (state[0] + ", " + state[1] + "TEST1");
-
 		return state;
 	}
 
 	public bool getStateBool(){
-		//Debug.Log (state[0] + ", " + state[1] + "TEST1");
-		return newState;
+        bool tempState = newState;
+        newState = false;
+		return tempState;
 	}
 }
